7c0e83359b8d1f044655c975c5cbe3262e48da76
Rachel Selkirk <rachel.selkirk@gmail.com>
2017-04-10 19:02:56 +0000

    created URL request

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,16 @@
 const request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+var GITHUB_USER = "rselkirk";
+var GITHUB_TOKEN = "0235b65ed5f8185df05efbf370be21ff319c3cbb";
+
+function getRepoContributors(repoOwner, repoName, cb) {
+ var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+ console.log(requestURL);
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
